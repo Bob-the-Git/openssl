86c3d999503fd593040fdc6f513e719c954f2446
Bob-the-Git <Bob-the-Git@outlook.com>
2022-09-21 22:04:29 +0800

    build-for-win.bat

---
+++ build-for-win.bat
@@ -0,0 +1,21 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+@echo on
+
+set OPENSSL_ROOT_DIR=%~dp0
+
+cd /d %OPENSSL_ROOT_DIR%
+mkdir out\build\Debug-Windows-AMD64
+cd out\build\Debug-Windows-AMD64
+perl %OPENSSL_ROOT_DIR%/Configure VC-WIN64A -static --prefix=%OPENSSL_ROOT_DIR%/out/install/Debug-Windows-AMD64 --openssldir=%OPENSSL_ROOT_DIR%/out/install/Debug-Windows-AMD64/data --debug >> build.log.txt 2>&1
+nmake >> build.log.txt 2>&1
+nmake test >> build.log.txt 2>&1
+nmake install >> build.log.txt 2>&1
+
+cd /d %OPENSSL_ROOT_DIR%
+mkdir out\build\Release-Windows-AMD64
+cd out\build\Release-Windows-AMD64
+perl %OPENSSL_ROOT_DIR%/Configure VC-WIN64A -static --prefix=%OPENSSL_ROOT_DIR%/out/install/Release-Windows-AMD64 --openssldir=%OPENSSL_ROOT_DIR%/out/install/Release-Windows-AMD64/data --release >> build.log.txt 2>&1
+nmake >> build.log.txt 2>&1
+nmake test >> build.log.txt 2>&1
+nmake install >> build.log.txt 2>&1
